305dd3b37c59abf04e249bcb4458160b0bd9e83d
kingsthere <kingsthere0@hotmail.com>
2023-05-27 12:40:02 +0800

    Adapt 0.0.93

--- common/src/main/kotlin/kingmc/platform/util/Formats.kt
+++ common/src/main/kotlin/kingmc/platform/util/Formats.kt
@@ -5,7 +5,6 @@ package kingmc.platform.util
 import kingmc.common.application.FormatCapableApplication
 import kingmc.common.application.WithApplication
 import kingmc.common.application.currentApplication
-import kingmc.common.text.Text
 import kingmc.util.KingMCDsl
 import kingmc.util.Utility
 import kingmc.util.format.*
@@ -19,29 +18,7 @@ import kingmc.util.format.*
  */
 @KingMCDsl
 @WithApplication
-fun String.format(vararg args: Any, formatStyle: FormatStyle = BracketStyle): String {
-    val application = currentApplication()
-    return if (application is FormatCapableApplication) {
-        this.formatWithContext(
-            formatStyle,
-            application.getFormatContext()
-                .with(ListFormatArguments(args.mapIndexed { index, any -> FormatArgument(any, index.toString()) }))
-        )
-    } else {
-        this.formatWithContext(formatStyle, ListFormatArguments(args.mapIndexed { index, any -> FormatArgument(any, index.toString()) }))
-    }
-}
-
-/**
- * Format the receiver with [args]
- *
- * @since 0.0.6
- * @author kingsthere
- * @receiver the string to format
- */
-@KingMCDsl
-@WithApplication
-fun Text.format(vararg args: Any, formatStyle: FormatStyle = BracketStyle): Text {
+fun String.format(vararg args: Any, formatStyle: Formatter = BracketStyle): String {
     val application = currentApplication()
     return if (application is FormatCapableApplication) {
         this.formatWithContext(
