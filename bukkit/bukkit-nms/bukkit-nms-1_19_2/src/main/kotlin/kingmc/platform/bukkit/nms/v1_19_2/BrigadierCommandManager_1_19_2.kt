35ff8c3911315e71952cc089e4104b2f3997a4fc
kingsthere <kingsthere0@hotmail.com>
2023-06-07 20:00:43 +0800

    Re-implemented BrigadierCommandManager_1_19_2.kt

--- bukkit/bukkit-nms/bukkit-nms-1_19_2/src/main/kotlin/kingmc/platform/bukkit/nms/v1_19_2/BrigadierCommandManager_1_19_2.kt
+++ bukkit/bukkit-nms/bukkit-nms-1_19_2/src/main/kotlin/kingmc/platform/bukkit/nms/v1_19_2/BrigadierCommandManager_1_19_2.kt
@@ -1,7 +1,6 @@
 package kingmc.platform.bukkit.nms.v1_19_2
 
 import kingmc.common.application.application
-import kingmc.common.application.withApplication
 import kingmc.common.context.annotation.Autowired
 import kingmc.common.context.annotation.Component
 import kingmc.common.context.annotation.Qualifier
@@ -17,7 +16,7 @@ import kingmc.platform.bukkit.brigadier.removeCommand
 import kingmc.platform.command.NAMESPACE_SEPARATOR
 import kingmc.platform.command.model.Command
 import kingmc.platform.command.model.Header
-import kingmc.platform.command.model.Node
+import kingmc.platform.facet.command.FacetCommandManager
 import kingmc.platform.version.ConditionalOnVersion
 
 @Component("brigadierCommandManager_1_19_2")
@@ -25,7 +24,7 @@ import kingmc.platform.version.ConditionalOnVersion
 @ConditionalOnBean(BrigadierNMS::class)
 @Scope(BeanScope.SINGLETON)
 @BukkitImplementation
-class BrigadierCommandManager_1_19_2 : BrigadierCommandManager, Releasable {
+class BrigadierCommandManager_1_19_2 : FacetCommandManager(), BrigadierCommandManager, Releasable {
     @Autowired
     @Qualifier("brigadierNMS_1_19_2")
     lateinit var brigadierNMS: BrigadierNMS_1_19_2
@@ -42,86 +41,57 @@ class BrigadierCommandManager_1_19_2 : BrigadierCommandManager, Releasable {
     override val defaultCommandNamespace: String
         get() = application.name
 
-    /**
-     * Close this command manager
-     */
-    override fun close() {
-        _registeredCommands.forEach {
-            unregister(it)
-        }
-        _registeredCommands.clear()
-    }
+    init {
+        register.before { command ->
+            val commandNode = command.data
+            if (commandNode is Header) {
+                try {
+                    val commandHeader = brigadierNMS.deserializeBrigadierCommandFromCommandHeader(commandNode)
+                    val builtCommandHeader = commandHeader.build()
+                    val commandDispatcher = brigadierNMS.getBrigadierDispatcher()
+                    // Register command with names without namespace
+                    commandDispatcher.root.addChild(builtCommandHeader)
+                    // Register command aliases
+                    commandHeader.aliases.forEach {
+                        commandDispatcher.root.addChild(AliasesCommandNode(it, builtCommandHeader))
+                    }
 
-    /**
-     * Get all registered commands in this command manager
-     */
-    override fun getRegisteredCommands(): List<Command<*>> {
-        return _registeredCommands
-    }
-
-    /**
-     * Remove a registered command from this command manager
-     */
-    override fun unregister(command: Command<*>) {
-        val commandNode = command.data
-        if (commandNode is Header) {
-            val commandDispatcher = brigadierNMS.getBrigadierDispatcher()
-
-            commandDispatcher.removeCommand(commandNode.name)
-            commandNode.aliases.forEach { alias ->
-                commandDispatcher.removeCommand(alias)
+                    // Register command with names with namespace
+                    val namespace = commandNode.namespace
+                    commandDispatcher.root.addChild(AliasesCommandNode("$namespace$NAMESPACE_SEPARATOR${commandHeader.literal}", builtCommandHeader))
+                    // Register command aliases
+                    commandHeader.aliases.forEach {
+                        commandDispatcher.root.addChild(AliasesCommandNode("$namespace$NAMESPACE_SEPARATOR$it", builtCommandHeader))
+                    }
+                } catch (e: Exception) {
+                    throw RuntimeException("An error occurred while registering command $commandNode", e)
+                }
+            } else {
+                throw IllegalArgumentException("BrigadierCommandManager can only register header command nodes")
             }
-            commandDispatcher.removeCommand("${commandNode.namespace}$NAMESPACE_SEPARATOR${commandNode.name}")
-            commandNode.aliases.forEach { alias ->
-                commandDispatcher.removeCommand("${commandNode.namespace}$NAMESPACE_SEPARATOR${alias}")
-            }
-        } else {
-            throw IllegalArgumentException("BrigadierCommandManager can only register header command nodes")
+            brigadierNMS.syncCommands()
+            _registeredCommands.add(command)
         }
-        brigadierNMS.syncCommands()
-        _registeredCommands.remove(command)
-    }
 
-    /**
-     * Remove a registered command from this command manager byb name
-     */
-    override fun unregister(name: String) {
-        // Remove the command by `name`
-        _registeredCommands.removeIf { (it.data as Node).name.startsWith(name) }
-    }
-
-    /**
-     * Add a registered command to this command manager
-     */
-    override fun register(command: Command<*>) {
-        val commandNode = command.data
-        if (commandNode is Header) {
-            try {
-                val commandHeader = brigadierNMS.deserializeBrigadierCommandFromCommandHeader(commandNode)
-                val builtCommandHeader = commandHeader.build()
+        unregister.before { command ->
+            val commandNode = command.data
+            if (commandNode is Header) {
                 val commandDispatcher = brigadierNMS.getBrigadierDispatcher()
-                // Register command with names without namespace
-                commandDispatcher.root.addChild(builtCommandHeader)
-                // Register command aliases
-                commandHeader.aliases.forEach {
-                    commandDispatcher.root.addChild(AliasesCommandNode(it, builtCommandHeader))
-                }
 
-                // Register command with names with namespace
-                val namespace = commandNode.namespace
-                commandDispatcher.root.addChild(AliasesCommandNode("$namespace$NAMESPACE_SEPARATOR${commandHeader.literal}", builtCommandHeader))
-                // Register command aliases
-                commandHeader.aliases.forEach {
-                    commandDispatcher.root.addChild(AliasesCommandNode("$namespace$NAMESPACE_SEPARATOR$it", builtCommandHeader))
+                commandDispatcher.removeCommand(commandNode.name)
+                commandNode.aliases.forEach { alias ->
+                    commandDispatcher.removeCommand(alias)
                 }
-            } catch (e: Exception) {
-                throw RuntimeException("An error occurred while registering command $commandNode", e)
+                commandDispatcher.removeCommand("${commandNode.namespace}$NAMESPACE_SEPARATOR${commandNode.name}")
+                commandNode.aliases.forEach { alias ->
+                    commandDispatcher.removeCommand("${commandNode.namespace}$NAMESPACE_SEPARATOR${alias}")
+                }
+            } else {
+                throw IllegalArgumentException("BrigadierCommandManager can only register header command nodes")
             }
-        } else {
-            throw IllegalArgumentException("BrigadierCommandManager can only register header command nodes")
+            brigadierNMS.syncCommands()
+            _registeredCommands.remove(command)
         }
-        brigadierNMS.syncCommands()
-        _registeredCommands.add(command)
     }
 
     /**
